888b50e1d5fac98ea87ae4fe824278628a2e327b
Kevin Grimm <kevin@grimmstales.us>
2014-10-14 09:02:08 -0400

    Added mock data

---
+++ HiddenMickeyProject/Areas/Editor/Controllers/LocationController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HiddenMickeyProject.Areas.Editor.Controllers
+{
+    public class LocationController : Controller
+    {
+
+    }
+}
