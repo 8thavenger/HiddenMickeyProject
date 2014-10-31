5ca66a69e1e293f8225b256515a66d216ec940de
8thAvenger <Kevin@grimmstales.us>
2014-10-30 21:36:22 -0400

    Build error correction

--- HiddenMickeyProject/Utilities/ObjectFactory.cs
+++ HiddenMickeyProject/Utilities/ObjectFactory.cs
@@ -5,6 +5,7 @@ using System.Reflection;
 using System.Xml;
 using HiddenMickeyProject.Data;
 using HiddenMickeyProject.Models;
+using System.Configuration;
 
 namespace HiddenMickeyProject.Utilities
 {
